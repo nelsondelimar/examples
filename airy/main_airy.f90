fff0124f5227cb6d3031af3dde9609851b857b09
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2021-03-30 15:02:38 -0300

    Updating files and thickness.dat output

--- airy/main_airy.f90
+++ airy/main_airy.f90
@@ -1,37 +1,41 @@
 	PROGRAM AIRY_THICKNESS
+
     IMPLICIT NONE
-    REAL(4), ALLOCATABLE :: MTOPO(:,:), XO(:), YO(:), ZO(:), THICK(:)
-    INTEGER ERRO_ALLOC,LABEL1
-	INTEGER NPOINTS, I, J
-	!--------------------------------------------------!
-	NPOINTS = 2601
+
+	REAL(4), ALLOCATABLE :: MTOPO(:,:), XO(:), YO(:), ZO(:), THICK(:)
+    INTEGER ERRO_ALLOC,LABEL1,LABEL2
+	INTEGER N, I, J
+
 	!--------------------------------------------------!
 	LABEL1 = 11
-	OPEN (UNIT = LABEL1, FILE = 'positiva.dat')
-	CLOSE (UNIT = LABEL1)
+	OPEN (UNIT = LABEL1, FILE = 'input.dat',action='read',status='old')
 	!--------------------------------------------------!
-	ALLOCATE (MTOPO(NPOINTS,3),STAT=ERRO_ALLOC)
-	ALLOCATE (XO(NPOINTS),STAT=ERRO_ALLOC)
-	ALLOCATE (YO(NPOINTS),STAT=ERRO_ALLOC)	
-	ALLOCATE (ZO(NPOINTS),STAT=ERRO_ALLOC)
-	ALLOCATE (THICK(NPOINTS),STAT=ERRO_ALLOC)
+	
+	N = 2601
 
-	DO I= 1,NPOINTS
+	ALLOCATE (MTOPO(N,3),STAT=ERRO_ALLOC)
+	ALLOCATE (XO(N),STAT=ERRO_ALLOC)
+	ALLOCATE (YO(N),STAT=ERRO_ALLOC)
+	ALLOCATE (ZO(N),STAT=ERRO_ALLOC)
+	ALLOCATE (THICK(N),STAT=ERRO_ALLOC)
+
+	DO I= 1,N
 	READ(LABEL1,*)(MTOPO(I,J), J = 1, 3)
     END DO
-	CLOSE (UNIT=LABEL1)
+
 	!--------------------------------------------------!
-	DO I = 1, NPOINTS
+	DO I = 1, N
     XO(I) = MTOPO(I,1)
-    YO(I) = MTOPO(I,2) 
+    YO(I) = MTOPO(I,2)
     ZO(I) = MTOPO(I,3)
-    PRINT*, I, XO(I), YO(I), ZO(I)
     END DO
-	!--------------------------------------------------!    
-	CALL THICKNESS_AIRY(ZO,NPOINTS,THICK)
+    print*, sum(zo)
+    print*, sum(zo)/N
 	!--------------------------------------------------!
-	!CALL WRITE_XYZ(XO, YO, THICK, NPOINTS, 'airy_thickness.dat')
-	!--------------------------------------------------!    
+	CALL THICKNESS_AIRY(ZO,N,THICK)
+	!--------------------------------------------------!
+	CALL WRITE_XYZ(XO, YO, THICK,N,'thickness.dat')
+	!--------------------------------------------------!
 	DEALLOCATE (MTOPO,STAT=ERRO_ALLOC)
 	DEALLOCATE (XO,STAT=ERRO_ALLOC)
 	DEALLOCATE (YO,STAT=ERRO_ALLOC)
